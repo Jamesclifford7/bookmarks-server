bcf6bbcfa90c3d8d10056ae9a11154175debe01c
James Clifford <jamesclifford7@gmail.com>
2020-10-27 16:54:12 -0700

    first pass

---
+++ src/store.js
@@ -0,0 +1,25 @@
+const bookmarks = [
+    {
+    title: "Google",
+    url: "http://www.google.com", 
+    rating: "1", 
+    description: "search engine", 
+    id: "123"
+    },
+    {
+      title: "Reddit",
+      url: "http://www.reddit.com", 
+      rating: "2", 
+      description: "forum", 
+      id: "456"
+    }, 
+    {
+      title: "Youtube", 
+      url: "http://www.youtube.com", 
+      rating: "3", 
+      description: "video", 
+      id: "789"
+    }
+  ];
+
+module.exports = { bookmarks }
